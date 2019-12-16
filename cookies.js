52bf7da3063ced0810d0381f043859d2d83541e1
Edward Akerboom <github@infostreams.net>
2019-12-16 14:38:59 +0100

    Initial commit

---
+++ cookies.js
@@ -0,0 +1,48 @@
+function notLoggedIn() {
+    document.getElementById( 'not-logged-in' ).setAttribute( 'style', 'display:flex' );
+
+    document.getElementById('login-button').addEventListener('click', () => {
+        window.open("https://www.coursera.org/?authMode=login", '_blank');
+        window.close();
+    })
+}
+
+function loggedIn( cookie ) {
+    document.getElementById( 'logged-in' ).setAttribute( 'style', 'display:flex' );
+
+    const button = document.getElementById( 'copy-button' );
+
+    button.addEventListener( 'click', e => {
+        navigator.clipboard.writeText( cookie.value ).then(
+            () => copySuccess(button),
+            () => copyFailure(button)
+        )
+    } )
+}
+
+function copySuccess(button) {
+    const countdown = (sec) => button.innerText = 'Copied the code! Closing in ' + sec + '...';
+    const limit = 4;
+    for ( let sec = limit; sec > 0; sec-- ) {
+        window.setTimeout(() => countdown(sec), (limit - sec) * 1000)
+    }
+    window.setTimeout( () => window.close(), limit * 1000)
+}
+
+function copyFailure(button) {
+    button.classList.add( 'button-error' );
+    button.innerText = 'Error! Could not copy the code.';
+}
+
+function init(cookies) {
+    cookies
+        .get( { url: "https://www.coursera.org", name: 'CAUTH' }, (cookie) => {
+            if ( !cookie ) {
+                notLoggedIn();
+            } else {
+                loggedIn( cookie );
+            }
+        })
+}
+
+init( typeof chrome === 'undefined' ? browser.cookies : chrome.cookies );
